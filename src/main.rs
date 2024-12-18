c1ded59e91a4db7c38c6447d42bb9a6de05f718b
Jacob Budzis <jrbudzis@gmail.com>
2024-12-17 20:12:38 -0800

    Prototype: Listen with TCP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // Send data to socket via `$ echo "foo" | nc localhost 8080`
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    match listener.accept() {
+        Ok((_socket, addr)) => println!("new client: {addr:?}"),
+        Err(e) => println!("cloudn't get client: {e:?}"),
+    }
 }

f63c3d5443d067cc267135b91435744bba5ae98d
Jacob Budzis <jrbudzis@gmail.com>
2025-03-28 23:29:39 -0700

    Add struct containing server state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 use std::net::TcpListener;
 
+enum Mode {
+    Leader{
+        // Volatile state (leader only)
+        // This state is re-initialized after every election.
+        next_index: Vec<u64>,  // The next log entry index to send to followers
+        match_index: Vec<u64>, // Index of highest log entry known to be replicated by each follower
+    },
+    Follower,
+    Candidate,
+}
+
+// TODO: Use the typestate pattern to provide compile-time guarantees about
+// the state of the Server. https://cliffle.com/blog/rust-typestate/
+pub struct Server {
+    // Persistent State  TODO: Make this persistent
+    // These fields are updated on stable storage before responding to messages.
+    current_term: u64,      // Latest term seen by the server
+    voted_for: Option<u64>, // Candidate which recieved vote in current term
+    log: Vec<u64>,          // State machine log entries
+
+    // Volatile State
+    commit_index: u64, // Index of highest log entry known to be committed
+    last_applied: u64, // Index of highest log entry applied to state machine
+    mode: Mode,        // The mode of operation of the server
+
+}
+
 fn main() {
     // Send data to socket via `$ echo "foo" | nc localhost 8080`
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();

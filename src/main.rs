dc47bbc558ec5155001acaea6faeeab4acde7de8
Jacob Budzis <jrbudzis@gmail.com>
2025-04-04 23:23:10 -0700

    Add non-functional TCP listener to binary entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use crate::server::Server;
 
 mod server;
@@ -5,5 +6,12 @@ mod watchdog;
 
 fn main() {
     let server = Server::new();
-    println!("{:?}", server)
+    println!("{:?}", server);
+
+    // Send data to socket via `$ echo "foo" | nc localhost 8080`
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    match listener.accept() {
+        Ok((_socket, addr)) => println!("new client: {addr:?}"),
+        Err(e) => println!("cloudn't get client: {e:?}"),
+    }
 }

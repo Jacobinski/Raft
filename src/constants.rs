f86c26763841a09ae807c399194b5e430d9e1e64
Jacob Budzis <jrbudzis@gmail.com>
2025-04-10 00:08:43 -0700

    Create constants file which defines LOCALHOST
    This will be used to share information between the client and server binaries.

---
+++ src/constants.rs
@@ -0,0 +1,1 @@
+pub const LOCALHOST:&'static str = "127.0.0.1:8080";
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::net::TcpListener;
 use crate::server::Server;
+use crate::constants::LOCALHOST;
 
 mod server;
 mod watchdog;
+mod constants;
 
 fn main() {
     let server = Server::new();
     println!("{:?}", server);
 
     // Send data to socket via `$ echo "foo" | nc localhost 8080`
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind(LOCALHOST).unwrap();
     match listener.accept() {
         Ok((_socket, addr)) => println!("new client: {addr:?}"),
         Err(e) => println!("cloudn't get client: {e:?}"),

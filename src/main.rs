625a22d323d0273cf867565b4fdccfb0a17caaa8
Jacob Budzis <jrbudzis@gmail.com>
2025-06-23 21:53:59 -0700

    Switch to async paradigm via Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use crate::server::Server;
 use raft::{constants::LOCALHOST, server::AppendEntriesRequest};
-use std::{fs::read, io::Read, net::TcpListener, vec};
+use tokio::{io::AsyncReadExt, net::TcpListener};
 
 mod server;
 mod watchdog;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: Connect the server to the TCP socket
     let server = Server::new();
     println!("{:?}", server);
 
     // Send data to socket via `$ echo "foo" | nc localhost 8080`
-    let listener = TcpListener::bind(LOCALHOST).unwrap();
-    match listener.accept() {
+    let listener = TcpListener::bind(LOCALHOST).await?;
+    match listener.accept().await {
         Ok((mut socket, addr)) => {
             println!("new client: {addr:?}");
-            let mut buffer = [0; 256];
-            let read_bytes = socket.read(&mut buffer)?;
+            let mut buffer = Vec::with_capacity(256);
+            let read_bytes = socket.read_buf(&mut buffer).await?;
             let request: AppendEntriesRequest =
                 serde_json::from_slice(&buffer[0..read_bytes]).unwrap();
             println!("{:?}", &request);
         }
         Err(e) => println!("cloudn't get client: {e:?}"),
     }
-
     Ok(())
 }

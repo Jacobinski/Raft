9a57ab89eb2b43490c9112aeb47ceab0626cf044
Jacob Budzis <jrbudzis@gmail.com>
2025-06-23 22:01:48 -0700

    Add infinite loop to Raft server request handler

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Send data to socket via `$ echo "foo" | nc localhost 8080`
     let listener = TcpListener::bind(LOCALHOST).await?;
-    match listener.accept().await {
-        Ok((mut socket, addr)) => {
-            println!("new client: {addr:?}");
-            let mut buffer = Vec::with_capacity(256);
-            let read_bytes = socket.read_buf(&mut buffer).await?;
-            let request: AppendEntriesRequest =
-                serde_json::from_slice(&buffer[0..read_bytes]).unwrap();
-            println!("{:?}", &request);
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, addr)) => {
+                println!("new client: {addr:?}");
+                let mut buffer = Vec::with_capacity(256);
+                let read_bytes = socket.read_buf(&mut buffer).await?;
+                let request: AppendEntriesRequest =
+                    serde_json::from_slice(&buffer[0..read_bytes]).unwrap();
+                println!("{:?}", &request);
+            }
+            Err(e) => println!("cloudn't get client: {e:?}"),
         }
-        Err(e) => println!("cloudn't get client: {e:?}"),
     }
-    Ok(())
 }
